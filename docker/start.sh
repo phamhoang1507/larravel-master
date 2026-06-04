f9423b7893cfdc5aa68b7d89f7a93ae8bdbc3ddb
hoangpd <hoangpd@miagi-so.net>
2026-06-04 16:35:24 +0700

    fix: gop nginx php-fpm cho render deploy

--- docker/start.sh
+++ docker/start.sh
@@ -2,4 +2,5 @@
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
+php artisan db:seed --force  ← thêm dòng này
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
